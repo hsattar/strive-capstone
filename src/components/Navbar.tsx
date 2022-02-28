1c2200a6322ada10e923e51b8b280adfcafe6093
Hasan Sattar <hasanraza1798@gmail.com>
2022-02-28 12:03:41 +0000

    improved navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,26 +12,30 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
+        <>
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded shadow-md">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
             <Link to="/" className="flex"><span className="self-center text-lg font-semibold whitespace-nowrap">Coding Buddy</span></Link>
             <div className="flex items-center">
-                <button onClick={() => setShowUserMenu(prev => !prev)} className="flex mr-3 text-sm rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300">
-                    <img className="w-8 h-8 rounded-full" src="" alt="" />
+                <button onClick={() => setShowUserMenu(prev => !prev)} className="flex mr-3 text-sm rounded-full md:mr-0">
+                    <img className="w-8 h-8 rounded-full" src="https://ui-avatars.com/api/?name=Hasan+Sattar" alt="" />
                 </button>
             </div>
             </div>
-            <div className={`${!showUserMenu && 'hidden'} z-50 my-4 text-base list-none bg-white rounded divide-y divide-gray-100 shadow`}>
+        </nav>
+        <div className="flex justify-end mr-4">
+            <div className={`${!showUserMenu && 'hidden'} z-50 my-1 text-base list-none bg-white rounded divide-y divide-gray-100 shadow-md w-40`}>
                 <div className="py-3 px-4">
                 <span className="block text-sm text-gray-900">Hasan Sattar</span>
                 <span className="block text-sm font-medium text-gray-500 truncate">hasan@sattar.com</span>
                 </div>
-                <ul className="py-1" aria-labelledby="dropdown">
+                <ul className="py-1">
                 <li><Link to="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Dashboard</Link></li>
                 <li><Link to="/" className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Settings</Link></li>
                 <li><button onClick={handleLogout} className="block w-full text-left py-2 px-4 text-sm text-red-400 hover:bg-gray-100">Logout</button></li>
                 </ul>
             </div>
-        </nav>
+        </div>
+        </>
     )
 }
